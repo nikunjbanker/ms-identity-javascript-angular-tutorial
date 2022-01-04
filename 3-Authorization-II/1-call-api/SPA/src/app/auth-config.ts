29014713026dde637eb8c1200ac5cbfa9829304f
Nikunj Banker <nikunj.banker@decos.com>
2022-01-04 23:36:57 +0530

    added tenant id, app id and scope etc based on azure ad app

--- 3-Authorization-II/1-call-api/SPA/src/app/auth-config.ts
+++ 3-Authorization-II/1-call-api/SPA/src/app/auth-config.ts
@@ -5,53 +5,53 @@
  * in app.module.ts file.
  */
 
-import { LogLevel, Configuration, BrowserCacheLocation } from '@azure/msal-browser';
+ import { LogLevel, Configuration, BrowserCacheLocation } from '@azure/msal-browser';
 
-const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1;
+ const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1;
 
-/**
- * Configuration object to be passed to MSAL instance on creation. 
- * For a full list of MSAL.js configuration parameters, visit:
- * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md 
- */
-export const msalConfig: Configuration = {
-  auth: {
-    clientId: 'Enter_the_Application_Id_Here', // This is the ONLY mandatory field that you need to supply.
-    authority: 'https://login.microsoftonline.com/Enter_the_Tenant_Info_Here', // Defaults to "https://login.microsoftonline.com/common"
-    redirectUri: '/', // Points to window.location.origin. You must register this URI on Azure portal/App Registration.
-  },
-  cache: {
-    cacheLocation: BrowserCacheLocation.LocalStorage, // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
-    storeAuthStateInCookie: isIE, // Set this to "true" if you are having issues on IE11 or Edge
-  },
-  system: {
-    loggerOptions: {
-      loggerCallback(logLevel: LogLevel, message: string) {
-        console.log(message);
-      },
-      logLevel: LogLevel.Verbose,
-      piiLoggingEnabled: false
-    }
-  }
-}
+ /**
+  * Configuration object to be passed to MSAL instance on creation.
+  * For a full list of MSAL.js configuration parameters, visit:
+  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
+  */
+ export const msalConfig: Configuration = {
+   auth: {
+     clientId: 'df3a2339-47ce-4d67-a618-d6cd4adff027', // This is the ONLY mandatory field that you need to supply.
+     authority: 'https://login.microsoftonline.com/92d28b9d-5f79-463c-84e6-ea7bc1f51af3', // Defaults to "https://login.microsoftonline.com/common"
+     redirectUri: '/', // Points to window.location.origin. You must register this URI on Azure portal/App Registration.
+   },
+   cache: {
+     cacheLocation: BrowserCacheLocation.LocalStorage, // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
+     storeAuthStateInCookie: isIE, // Set this to "true" if you are having issues on IE11 or Edge
+   },
+   system: {
+     loggerOptions: {
+       loggerCallback(logLevel: LogLevel, message: string) {
+         console.log(message);
+       },
+       logLevel: LogLevel.Verbose,
+       piiLoggingEnabled: false
+     }
+   }
+ }
 
-/**
- * Add here the endpoints and scopes when obtaining an access token for protected web APIs. For more information, see:
- * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
- */
-export const protectedResources = {
-  todoListApi: {
-    endpoint: "https://localhost:44351/api/todolist",
-    scopes: ["Enter_the_Web_Api_Scope_here"],
-  },
-}
+ /**
+  * Add here the endpoints and scopes when obtaining an access token for protected web APIs. For more information, see:
+  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
+  */
+ export const protectedResources = {
+   todoListApi: {
+     endpoint: "https://localhost:44351/api/todolist",
+     scopes: ["api://e71d8ac1-3048-4a9b-921a-922f198f302c/access_as_user"],
+   },
+ }
 
-/**
- * Scopes you add here will be prompted for user consent during sign-in.
- * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
- * For more information about OIDC scopes, visit: 
- * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
- */
-export const loginRequest = {
-  scopes: []
-};
+ /**
+  * Scopes you add here will be prompted for user consent during sign-in.
+  * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+  * For more information about OIDC scopes, visit:
+  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
+  */
+ export const loginRequest = {
+   scopes: []
+ };
